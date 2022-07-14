a3546310536bdebb54945d8d8f31d80160f6eda1
mastrmind-dev <asapthaka@gmail.com>
2022-07-14 16:59:33 +0530

    chat messages area is placed between chat header and text input

--- client/src/App.js
+++ client/src/App.js
@@ -27,11 +27,47 @@ const App = () => {
   };
   return (
     <div className="row">
-      <div className="chatbot-boundary col s3 right" style={{boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px", position:'fixed', height: '70%', width:'25%', overflow:'auto'}}>
-        <div className="chatbot-messages">{<Message message={message} />}</div>
-        <div className="text-input">
+      <div
+        className="chatbot-boundary col s3 right"
+        style={{
+          boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+          position: "fixed",
+          height: "70%",
+          width: "25%",
+          overflow: "auto",
+          right: "0",
+          bottom: "0",
+        }}
+      >
+        <div
+          className="chatbot-header"
+          style={{
+            position: "fixed",
+            height: "7%",
+            width: "25%",
+            right:'0',
+            backgroundColor: "pink",
+            zIndex:'99'
+          }}
+        >
+          Chatty
+        </div>
+        <div className="chatbot-messages" style={{backgroundColor:'red', position:'relative', marginTop:'14%', marginBottom:'14%', paddingBottom:'3%'}}>
+          {<Message message={message} />}
+        </div>
+        <div
+          className="text-input"
+          style={{
+            position: "fixed",
+            width: "25%",
+            right: "0",
+            bottom: "0",
+            zIndex: "99",
+            height: "7%",
+            backgroundColor: "pink",
+          }}
+        >
           <input
-            style={{ position: "fixed", top: "100px", backgroundColor: "pink" }}
             type="text"
             onKeyPress={(e) => {
               if (e.key === "Enter") {
