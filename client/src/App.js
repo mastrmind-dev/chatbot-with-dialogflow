e04b05f8dac07920e1a66e18b3155bc775a3799f
mastrmind-dev <asapthaka@gmail.com>
2022-07-16 22:53:25 +0530

    add a close button and chatbot icon to open the chatbot

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import Message from "./Message";
-import { Slider } from "materialize-css";
 
 const App = () => {
   const [message, setMessage] = useState([]);
   const [userMessageSent, setUserMessageSent] = useState(false);
+  const [isClosed, setIsClosed] = useState(true);
   const chatAreaBottom = useRef(null);
   const inputText = useRef(null);
-  const userSaid = useRef('')
+  const userSaid = useRef("");
 
   let says;
 
   useEffect(() => {
-    chatAreaBottom.current.scrollIntoView({ behavior: "smooth" });
-    inputText.current.focus();
+    if (!isClosed) {
+      chatAreaBottom.current.scrollIntoView({ behavior: "smooth" });
+      // inputText.current.focus();
+    }
   }, [message]);
 
   const userMessage = async (userInput) => {
@@ -26,30 +28,13 @@ const App = () => {
 
     setMessage([...message, says]);
     setUserMessageSent(true);
-
-    // var chatArray = [says];
-
-    // // const response = await axios.post(
-    // //   "http://localhost:4000/api/df_text_query",
-    // //   { text: says.msg }
-    // // );
-
-    // says = {
-    //   speaks: "bot",
-    //   // msg: response.data,
-    //   msg: "hello buddy",
-    // };
-    // chatArray.push(says);
-    // setMessage([...message, ...chatArray]);
   };
 
   const botMessage = async (userInput) => {
-    console.log(typeof(userInput))
     const response = await axios.post(
       "http://localhost:4000/api/df_text_query",
       { text: userInput }
     );
-    console.log(response.data);
     says = {
       speaks: "bot",
       msg: response.data,
@@ -57,102 +42,121 @@ const App = () => {
     setMessage([...message, says]);
     setUserMessageSent(false);
   };
-//We are in a functional component. So below if code block is executed automatically. No need to put it into a nested function.
-  if(userMessageSent){
-    console.log(userMessageSent)
-    console.log(userSaid)
-    botMessage(userSaid.current)
+  //We are in a functional component. So below if code block is executed automatically. No need to put it into a nested function.
+  if (userMessageSent) {
+    botMessage(userSaid.current);
   }
 
   return (
     <div className="row">
-      <div
-        className="chatbot-boundary col s3 right"
-        style={{
-          boxShadow:
-            "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
-          position: "fixed",
-          height: "70%",
-          width: "25%",
-          right: "2%",
-          bottom: "3%",
-          overflowY: "auto",
-          borderRadius: "10px",
-        }}
-      >
-        <div
-          className="chatbot-header"
-          style={{
-            position: "fixed",
-            height: "7%",
-            width: "25%",
-            right: "2%",
-            backgroundColor: "#0d47a1",
-            zIndex: "99",
-            paddingLeft: "1%",
-            paddingTop: "0.7%",
-            color: "white",
-            borderTopLeftRadius: "10px",
-            borderTopRightRadius: "10px",
+      {isClosed ? (
+        <a
+          className="btn-floating btn-large waves-effect waves-light blue"
+          style={{ position: "fixed", right: 40, bottom: 40 }}
+          onClick={() => {
+            setIsClosed(false);
           }}
         >
-          Chatty
-        </div>
+          <i className="material-icons">chat</i>
+        </a>
+      ) : (
         <div
-          className="chatbot-messages"
+          className="chatbot-boundary col s3 right"
           style={{
-            // backgroundColor: "red",
-            position: "relative",
-            marginTop: "14%",
-            marginBottom: "14%",
-            paddingBottom: "3%",
-            zIndex: "9",
-          }}
-        >
-          {/* {<Message message={message} />} */}
-          {userMessageSent ? (
-            <>
-              <Message message={message} />
-              <div>chatty is typing...</div>
-            </>
-          ) : (
-            <Message message={message} />
-          )}
-          <div className="chatAreaBottom" ref={chatAreaBottom}></div>
-          {/**this empty div is very important for scrollIntoView in the useEffect hook. To scroll this div into view the chatbot */}
-        </div>
-        <div
-          className="text-input"
-          style={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
             position: "fixed",
+            height: "70%",
             width: "25%",
             right: "2%",
             bottom: "3%",
-            zIndex: "99",
-            height: "7%",
-            backgroundColor: "#0d47a1",
-            paddingLeft: "1%",
-            paddingRight: "1%",
-            borderBottomLeftRadius: "10px",
-            borderBottomRightRadius: "10px",
+            overflowY: "auto",
+            borderRadius: "10px",
           }}
         >
-          <input
+          <div
+            className="chatbot-header"
             style={{
+              position: "fixed",
+              height: "7%",
+              width: "25%",
+              right: "2%",
+              backgroundColor: "#0d47a1",
+              zIndex: "99",
+              paddingLeft: "1%",
+              paddingTop: "0.7%",
               color: "white",
+              borderTopLeftRadius: "10px",
+              borderTopRightRadius: "10px",
             }}
-            ref={inputText}
-            type="text"
-            onKeyPress={(e) => {
-              if (e.key === "Enter") {
-                userSaid.current=e.target.value
-                console.log(userSaid.current)
-                return userMessage(userSaid.current);
-              }
+          >
+            Chatty
+            <a
+              className="waves-effect waves-light btn-small right blue"
+              style={{ marginRight: "2%" }}
+              onClick={() => {
+                setIsClosed(true);
+              }}
+            >
+              <i className="material-icons">close</i>
+            </a>
+          </div>
+          <div
+            className="chatbot-messages"
+            style={{
+              // backgroundColor: "red",
+              position: "relative",
+              marginTop: "14%",
+              marginBottom: "14%",
+              paddingBottom: "3%",
+              zIndex: "9",
             }}
-          />
+          >
+            {/* {<Message message={message} />} */}
+            {userMessageSent ? (
+              <>
+                <Message message={message} />
+                <div>chatty is typing...</div>
+              </>
+            ) : (
+              <Message message={message} />
+            )}
+            <div className="chatAreaBottom" ref={chatAreaBottom}></div>
+            {/**this empty div is very important for scrollIntoView in the useEffect hook. To scroll this div into view the chatbot */}
+          </div>
+          <div
+            className="text-input"
+            style={{
+              position: "fixed",
+              width: "25%",
+              right: "2%",
+              bottom: "3%",
+              zIndex: "99",
+              height: "7%",
+              backgroundColor: "#0d47a1",
+              paddingLeft: "1%",
+              paddingRight: "1%",
+              borderBottomLeftRadius: "10px",
+              borderBottomRightRadius: "10px",
+            }}
+          >
+            <input
+              style={{
+                color: "white",
+              }}
+              autoFocus
+              ref={inputText}
+              type="text"
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  userSaid.current = e.target.value;
+                  return userMessage(userSaid.current);
+                }
+              }}
+            />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
